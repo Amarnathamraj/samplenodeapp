677661447a1d3a8703167f96e455f269475be187
Amarnathamraj <amarnath987654@gmail.com>
2025-05-30 21:44:43 +0530

    mynodeapp

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+app.get('/',(req,resp)=>{
+    resp.json({message:'get request received'})
+})
+
+app.get('/all',(req,resp)=>{
+      resp.json({message:'get request made '})
+})
+
+app.post('/add',(req,resp)=>{
+    console.log(req.body)
+    resp.json({message:'post request received',data:req.body})
+})
+
+const PORT=process.env.PORT||8080;
+app.listen(PORT,(req,res)=>{
+    console.log('app is listening on port no',PORT)
+})
